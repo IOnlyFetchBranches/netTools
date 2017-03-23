4764ee7ed6c3d9c87a92046c46f5562fa0609b45
demarcus-joachim <llmuzical@gmail.com>
2017-03-23 16:51:53 -0400

    Newest (With working network connections and its transmittin g bytecode, just need it to decode the text in real time
    Signed-off-by: demarcus-joachim <llmuzical@gmail.com>

--- src/clientApplet.java
+++ src/clientApplet.java
@@ -1,6 +1,11 @@
 import Tools.printTools;
 
-import java.net.InetAddress;
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 import static Tools.printTools.*;
@@ -9,6 +14,7 @@ public class clientApplet {
     private static String address,userName,password;
     private static int port;
     private static InetAddress serverAddress;
+    private static Socket clientSocket;
     public static void main(String[] args){
         try{
             if(args[0].equalsIgnoreCase("help")){
@@ -29,22 +35,99 @@ public class clientApplet {
                                 Integer.parseInt(st.nextToken());
                             }
                             serverAddress=InetAddress.getByName(address);
+
+                            //test Connection
                             if(serverAddress.isReachable(5000)) {
                                 println(serverAddress.toString());
-                                println("Address is REACHABLE");
+                                println("Address is REACHABLE!");
                             }
                             else{
                                 println(serverAddress.toString());
-                                errln("Address is NOT REACHABLE");
+                                errln("Address is NOT REACHABLE!");
                         }
                         }
                         else if(address.contains("www") ||address.contains(".")){
-                            println(address);
+                            serverAddress=InetAddress.getByName(address);
 
+                            //test Connection
+                            if(serverAddress.isReachable(5001)) {
+                                println(serverAddress.toString());
+                                println("Address is REACHABLE!");
+                            }
+                            else {
+                                println(serverAddress.toString());
+                                errln("Address is NOT REACHABLE!");
 
+                            }
                         }
                     }
+
+                    if(x==1){
+                        port=Integer.parseInt(args[x]);
+                        clientSocket=new Socket();
+                        SocketAddress hostName=new InetSocketAddress(serverAddress.toString().substring(1,serverAddress.toString().length()),port);
+                        println("Attempting to connect to server at->"+hostName.toString());
+                        try {
+                            clientSocket.connect(hostName, 5000);
+                        }catch(SocketTimeoutException |ConnectException e){
+                            errln("Timeout/Host Unreachable");
+                        }
+
+
+                        //Input handler Thread
+                        Thread conversationEngine=new Thread(()->{
+                            try {
+                                InputStream inbox=clientSocket.getInputStream();
+                                StringBuilder incomingMessage=new StringBuilder();
+
+                                while (Thread.currentThread().isAlive()) {
+                                    if (inbox !=null){
+                                        incomingMessage.append((char)inbox.read());
+                                    }
+
+                                }
+                                println(incomingMessage.toString());
+                                incomingMessage.delete(0,incomingMessage.length()-1);
+                            }catch(IOException ioe){
+                                errprint(ioe.getMessage());
+                            }
+                        });
+                        conversationEngine.setDaemon(true);
+                        conversationEngine.start();
+                        try{
+                            Thread.sleep(2000);
+                            println("\nConnection Established...\n");
+                        }catch(Exception e){errprint("Sleep Error");};
+
+
+
+
+
+
+                        //messaging handlers
+                        String message;
+                        //output
+                        while (Thread.currentThread().isAlive()) {
+                            message = new Scanner(System.in).next();
+                            //errln(message); //debug line
+
+                            //check for exit
+                            if(message.equalsIgnoreCase("exit")){System.exit(0);}
+                            //send message;
+                            clientSocket.getOutputStream().write(message.getBytes());
+
+                        }
+
+
+                    }
                 }
+
+
+
+
+
+
+
             }catch(Exception e){
                 errln("Seems Like you've incorrectly done your Arguments, for help, just execute java appName help...\n ");
 
--- src/serverApp.java
+++ src/serverApp.java
@@ -1,3 +1,5 @@
+import javafx.scene.layout.Priority;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,170 +11,203 @@ import java.net.Socket;
 import java.util.Scanner;
 
 import static Tools.printTools.*;
+import static java.lang.Thread.currentThread;
 
 
-/**
- * Created by demarcus-joachim on 3/23/2017.
- */
 public class serverApp {
     static int portnumber;
-
+    public static boolean establishedConnection=false;
 
 
     public static void main(String[] args){
         //master loop +control
         boolean reset;
-        do{
-            reset=false;
+        do {
+            reset = false;
 
             println("Welcome to the Server!\n");
-            try{
+            try {
                 Thread.sleep(1500);
                 print("Getting Port");
-                for(int x=0;x<3;x++){
+                for (int x = 0; x < 3; x++) {
                     Thread.sleep(300);
-                        try{
-                            Integer.parseInt(args[0]);
-                            if(x==2){
-                                print(".\n");
-                            }
-                            else {
-                                print(".");
-                            }
-                        }catch(Exception e) {
-                            errprint(".");
+                    try {
+                        Integer.parseInt(args[0]);
+                        if (x == 2) {
+                            print(".\n");
+                        } else {
+                            print(".");
                         }
-
-
-
-                }
-            }catch(Exception e){errln(e.getMessage());}
-
-
-        //Grab PortNumber or Request IT.
-        boolean portError=false;
-        do {
-            try {
-                if(portError){
-                    portError=false;
-                    portnumber=new Scanner(System.in).nextInt();
-                }
-                else{
-                    portnumber = Integer.parseInt(args[0]);
-                }
-            }catch(Exception e){
-                portError=true;
-                System.err.println("\nError with port, or is not a Number, Please Re-enter");
-            }
-        }while(portError);
-
-        //declare server socket
-        ServerSocket serverSocket=null;
-        try {
-            //bind the port
-            try {
-                serverSocket = new ServerSocket(portnumber);
-            } catch (IOException ioe) {
-                System.err.println("Port is in use, Sorry! Try Again? [y/n]");
-                boolean correct = true;
-                do {
-                    String choice = new Scanner(System.in).next();
-                    boolean isY = choice.equalsIgnoreCase("y");
-                    boolean isN = choice.equalsIgnoreCase("n");
-                    switch (isY + "-" + isN) {
-                        case "true-false":
-                            if (!correct) {
-                                correct = true;
-                            }
-                            reset = true;
-                            throw new Exception("Reset-Bypass");
-                        case "false-true":
-                            System.exit(0);
-                            break;
-                        default:
-                            System.err.println("Invalid!");
-                            correct = false;
+                    } catch (Exception e) {
+                        errprint(".");
                     }
-                } while (!correct);
+
+
+                }
+            } catch (Exception e) {
+                errln(e.getMessage());
             }
 
+
+            //Grab PortNumber or Request IT.
+            boolean portError = false;
+            do {
+                try {
+                    if (portError) {
+                        portError = false;
+                        portnumber = new Scanner(System.in).nextInt();
+                    } else {
+                        portnumber = Integer.parseInt(args[0]);
+                    }
+                } catch (Exception e) {
+                    portError = true;
+                    System.err.println("\nError with port, or is not a Number, Please Re-enter");
+                }
+            } while (portError);
+
+            //declare server socket
+            ServerSocket serverSocket = null;
             try {
-
-
-                   String address = InetAddress.getLocalHost().toString();
-
-
-
-                //run waiter Thread;
-                Thread wait = new Thread(() -> {
-                    int index = 0;
-                    int dot = 0;
-                    boolean done = false;
-                    while (!done) {
-
-                        if (index == 60) {
-                            println("\nTimeout has occurred, to Retry Connection type \"y\", Else the program will Exit!");
-                            String choice = new Scanner(System.in).next();
-                            if ((choice.contains("y")) || (choice.contains("Y"))) {
-                                index = 0;
-                            } else {
-                                done = true;
+                //bind the port
+                try {
+                    serverSocket = new ServerSocket(portnumber);
+                } catch (IOException ioe) {
+                    System.err.println("Port is in use, Sorry! Try Again? [y/n]");
+                    boolean correct = true;
+                    do {
+                        String choice = new Scanner(System.in).next();
+                        boolean isY = choice.equalsIgnoreCase("y");
+                        boolean isN = choice.equalsIgnoreCase("n");
+                        switch (isY + "-" + isN) {
+                            case "true-false":
+                                if (!correct) {
+                                    correct = true;
+                                }
+                                reset = true;
+                                throw new Exception("Reset-Bypass");
+                            case "false-true":
                                 System.exit(0);
-                            }
+                                break;
+                            default:
+                                System.err.println("Invalid!");
+                                correct = false;
                         }
+                    } while (!correct);
+                }
 
-                        try {
-                            //Runtime.getRuntime().exec("cls");
-                            println("Server is now listening on port:" + portnumber + " IP->" + address);
-                            for (int x = 0; x < 60; x++) {
-                                index++;
+                try {
+
+
+                    String address = InetAddress.getLocalHost().toString();
+
+
+                    //run waiter Thread;
+                    Thread wait = new Thread(() -> {
+                        int index = 0;
+                        int dot = 0;
+                        boolean done = false;
+                        println(currentThread().toString());
+                        while (!done) {
+                            if (establishedConnection)
+                                break;
+
+                            else {
                                 if (index == 60) {
-
-                                    break;
+                                    println("\nTimeout has occurred, to Retry Connection type \"y\", Else the program will Exit!");
+                                    String choice = new Scanner(System.in).next();
+                                    if ((choice.contains("y")) || (choice.contains("Y"))) {
+                                        index = 0;
+                                    } else {
+                                        done = true;
+                                        System.exit(0);
+                                    }
                                 }
 
                                 try {
-                                    Thread.sleep(1000);
-                                    if (index % 10 == 0) {
-                                        print(60 - index + "");
-                                        println("");
-                                        continue;
-                                    }
-                                    print(60 - index + "...");
+                                    //Runtime.getRuntime().exec("cls");
+                                    println("Server is now listening on port:" + portnumber + " IP->" + address);
+                                    for (int x = 0; x < 60; x++) {
 
+                                        if (index == 60 || establishedConnection) {
+
+                                            break;
+                                        }
+                                        index++;
+
+                                        try {
+
+                                            Thread.sleep(1000);
+                                            if (index % 10 == 0) {
+                                                print(60 - index + "");
+                                                println("");
+                                                continue;
+                                            }
+                                            print(60 - index + "...");
+
+                                        } catch (Exception e) {
+                                            e.getMessage();
+                                        }
+                                    }
                                 } catch (Exception e) {
-                                    errln(e.getMessage());
+                                    e.getCause();
                                 }
                             }
-                        } catch (Exception e) {
-                            e.getCause();
                         }
-                    }
-                });
-                wait.setDaemon(true);
-                wait.run();
+                    });
 
 
-                //initiate listening
+                    wait.setDaemon(true);
+                    wait.start();
 
-                Socket clientSocket = serverSocket.accept();
-                if (wait.isAlive()) {
-                    wait.interrupt();
-                    println("Connection Established");
-                }
+
+                    //Start listening
+                    Socket clientSocket = serverSocket.accept();
+
+                    establishedConnection = true;
+
+
+
+
+                    //wait for other thread to kill itself
+                    Thread.sleep(5000);
+
+                    println("\n\nConnection Established [" + clientSocket.getInetAddress().toString() + "]");
+
                 PrintWriter outToClient =
                         new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader inToClient = new BufferedReader(
+                BufferedReader inFromClient = new BufferedReader(
                         new InputStreamReader(clientSocket.getInputStream()));
+                String message = "empty";
+
+
+                outToClient.println("Hello! Welcome, to leave just type exit :)");
+                println("\nHello! Welcome, to leave just type exit :)");
+
+
+
+
+                //messaging handlers
+
+                    //output
+                while (Thread.currentThread().isAlive()) {
+                    message = new Scanner(System.in).next();
+                    //errln(message); //debug line
+
+                    //check for exit
+                    if(message.equalsIgnoreCase("exit")){System.exit(0);}
+                    //send message;
+                    outToClient.println(message);
+
+                }
 
 
             } catch (IOException e) {
                 errln("Error binding clientSocket! -> " + e.getCause() + " " + e.getLocalizedMessage() + " in " + e.getClass());
                 reset = true;
             } catch (Exception e) {
-                print("Restarting...");
-            }
+            e.getCause();
 
+        }
 
         }catch(Exception e){
             e.getCause();
